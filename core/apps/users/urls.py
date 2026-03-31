eaee316c7dda962a233ffe265a16646a6a161e97
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2026-03-31 16:37:11 +0500

    fix

--- core/apps/users/urls.py
+++ core/apps/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from core.apps.users.views.register import RegisterAPIView
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
 ]
