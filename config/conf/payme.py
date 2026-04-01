7f7cb59e67d5fbf45934533d3a49c61ac2a901bc
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2026-04-01 18:18:18 +0500

    fix

--- config/conf/payme.py
+++ config/conf/payme.py
@@ -1,6 +1,6 @@
 PAYME_ID = "69cbf4a0eca8f515237e1ab8"
 PAYME_KEY = "?2@i65YiDZk88Gu3&pRT9eOg&HbvnAWds0iN"
-PAYME_ACCOUNT_FIELD = "order_id"
+PAYME_ACCOUNT_FIELD = "id"
 PAYME_AMOUNT_FIELD = "total_price"
 PAYME_ACCOUNT_MODEL = "core.apps.shared.models.Order"
 PAYME_ONE_TIME_PAYMENT = True
