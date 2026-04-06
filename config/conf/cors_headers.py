ef5c8d8c18aeb58a359db05ec57e8834760b9db5
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2026-04-06 16:54:18 +0500

    add cors

--- config/conf/cors_headers.py
+++ config/conf/cors_headers.py
@@ -4,6 +4,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "https://plagat.vercel.app",
     "https://anti-plagiat.uz",
+    "https://dev.anti-plagiat.uz",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -14,4 +15,6 @@ CSRF_TRUSTED_ORIGINS = [
     "https://plagat.vercel.app",
     "https://api.anti-plagiat.uz",
     "https://anti-plagiat.uz",
+    "https://dev-api.anti-plagiat.uz",
+    "https://dev.anti-plagiat.uz",
 ]
