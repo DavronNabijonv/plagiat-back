6ea056aa372cfe5624b80965caf4ed4bd02a27c5
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2026-04-08 14:27:24 +0500

    fix

--- core/apps/shared/admin/document.py
+++ core/apps/shared/admin/document.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 from core.apps.shared.models import Document, DocumentResult
 
 
-admin.site.register(Document)
-admin.site.register(DocumentResult)
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('user',)
+
+@admin.register(DocumentResult)
+class DocumentResultAdmin(admin.ModelAdmin):
+    list_display = ('id', 'document', 'status', 'created_at')
+    search_fields = ('document__name',)
