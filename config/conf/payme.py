06c316c8ae0a10f5954f96c6565bfae33919a870
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2026-04-08 20:12:00 +0500

    fix

--- config/conf/payme.py
+++ config/conf/payme.py
@@ -1,5 +1,8 @@
-PAYME_ID = "69cbf4a0eca8f515237e1ab8"
-PAYME_KEY = "u5gfRwvRc3qNxPw#@hV2pFH#ejA4XI&TK%7j"
+from config.env import env
+
+
+PAYME_ID = env.str("PAYME_ID")
+PAYME_KEY = env.str("PAYME_KEY")
 PAYME_ACCOUNT_FIELD = "id"
 PAYME_AMOUNT_FIELD = "total_price"
 PAYME_ACCOUNT_MODEL = "core.apps.shared.models.Order"
