735eedd13063b1f161e522ffced94f62c520356c
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2026-04-08 14:29:17 +0500

    change the admin panel fields

--- core/apps/shared/admin/document.py
+++ core/apps/shared/admin/document.py
@@ -5,11 +5,11 @@ from core.apps.shared.models import Document, DocumentResult
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'created_at')
-    search_fields = ('name',)
+    list_display = ('id', 'title', 'created_at')
+    search_fields = ('title',)
     list_filter = ('user',)
 
 @admin.register(DocumentResult)
 class DocumentResultAdmin(admin.ModelAdmin):
-    list_display = ('id', 'document', 'status', 'created_at')
-    search_fields = ('document__name',)
+    list_display = ('id', 'document', 'created_at')
+    search_fields = ('document__title',)
