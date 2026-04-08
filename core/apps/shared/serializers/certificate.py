0901a60e17163e180a758eadc8177e2811c6eae1
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2026-04-08 18:11:30 +0500

    fix

--- core/apps/shared/serializers/certificate.py
+++ core/apps/shared/serializers/certificate.py
@@ -32,10 +32,6 @@ class CertificateDownloadSerializer(serializers.Serializer):
             raise serializers.ValidationError(
                 "Hujjat mavjud emas."
             )
-        if not document.certificate:
-            raise serializers.ValidationError(
-                "Sertifikat yuklab olish uchun to'lov qiling. 20600.00 so'm"
-            )
         if not document.certificate_file:
             raise serializers.ValidationError(
                 "Sertifikat tayyor emas, biroz kuting."
